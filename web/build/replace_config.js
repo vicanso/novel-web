cce2dd7f00b7fb655d95c8bf799342912359679f
vicanso <vicansocanbico@gmail.com>
2018-11-08 21:33:47 +0800

    chore: 调整构建脚本

---
+++ web/build/replace_config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const indexFile = path.join(__dirname, '../dist/index.html');
+const html = fs.readFileSync(indexFile, 'utf8');
+
+const config = `
+        window.CONFIG = {
+          "env": "production",
+          "urlPrefix": "https://papanovel.com/@nv",
+          "coverUrlPrefix": "https://tiny.papanovel.com/api/images/v1"
+        };
+`;
+const newHtml = html.replace(/\/\/ CONFIG START([\s\S]+)\/\/ CONFIG END/, config);
+fs.writeFileSync(indexFile, newHtml);
